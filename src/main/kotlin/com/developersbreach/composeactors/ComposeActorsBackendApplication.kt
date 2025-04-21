b69c5f01e34763ce2890924f898ff3673207226b
Raj <rajashekarraju123@gmail.com>
2025-04-21 15:01:38 +0530

    Initial spring boot and project configuration

---
+++ src/main/kotlin/com/developersbreach/composeactors/ComposeActorsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.developersbreach.composeactors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComposeActorsBackendApplication
+
+fun main() {
+	runApplication<ComposeActorsBackendApplication>()
+}
---
+++ src/test/kotlin/com/developersbreach/composeactors/ComposeActorsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.developersbreach.composeactors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComposeActorsBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
